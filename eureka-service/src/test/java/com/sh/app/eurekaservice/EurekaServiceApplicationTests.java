3917fdd8338b125d5caf22d620e5af201e152455
Samuel <shmenzala@outlook.com>
2023-01-20 16:15:03 -0500

    Eureka

---
+++ eureka-service/src/test/java/com/sh/app/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.app.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- vehiculo-service/src/main/java/com/sh/app/vehiculoservice/VehiculoServiceApplication.java
+++ vehiculo-service/src/main/java/com/sh/app/vehiculoservice/VehiculoServiceApplication.java
@@ -2,8 +2,10 @@ package com.sh.app.vehiculoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VehiculoServiceApplication {
 
 	public static void main(String[] args) {
