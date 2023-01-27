46730a6178be44e5ff81ed046d58c2b539346702
Samuel <shmenzala@outlook.com>
2023-01-27 01:28:08 -0500

    usu yaml eureka main

--- eureka-service/src/main/java/com/sh/app/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/sh/app/eurekaservice/EurekaServiceApplication.java
@@ -2,10 +2,10 @@ package com.sh.app.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
