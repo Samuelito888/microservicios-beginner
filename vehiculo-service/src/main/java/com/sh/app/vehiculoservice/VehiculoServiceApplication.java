08bb526b366f91444cddb8dbdfa6f0686cd17926
Samuel <shmenzala@outlook.com>
2023-01-27 16:06:10 -0500

    eure yaml

--- vehiculo-service/src/main/java/com/sh/app/vehiculoservice/VehiculoServiceApplication.java
+++ vehiculo-service/src/main/java/com/sh/app/vehiculoservice/VehiculoServiceApplication.java
@@ -2,10 +2,10 @@ package com.sh.app.vehiculoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class VehiculoServiceApplication {
 
 	public static void main(String[] args) {
